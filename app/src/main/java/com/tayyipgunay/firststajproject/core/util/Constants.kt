41c254498e91e6b5bb21dce9bc147cf4e5ce89f6
Tayyip Günay <91159336+tayyipgunay@users.noreply.github.com>
2025-10-09 12:53:49 +0300

    Update Constants.kt

--- app/src/main/java/com/tayyipgunay/firststajproject/core/util/Constants.kt
+++ app/src/main/java/com/tayyipgunay/firststajproject/core/util/Constants.kt
@@ -1,7 +1,6 @@
 package com.tayyipgunay.firststajproject.core.util
 
 object Constants {
-    const val BASE_URL = "http://37.156.246.102:9082/"
   
     const val PRODUCTS = "products"
     const val ADD_PRODUCT = "addProduct"
